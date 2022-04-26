c563ae685906f953e9d6f3c0a923a30ff86e9660
= <=>
2022-04-26 14:30:37 +0200

    Created add and concatAll method in TableList

--- src/main/java/ua/palamar/table/TableList.java
+++ src/main/java/ua/palamar/table/TableList.java
@@ -1,4 +1,21 @@
 package ua.palamar.table;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TableList {
+
+    private final List<String> tables = new ArrayList<>();
+
+    public void add(String table) {
+        tables.add(table);
+    }
+
+    public String concatAll() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (String table : tables) stringBuilder.append(table);
+
+        return stringBuilder.toString();
+    }
 }
